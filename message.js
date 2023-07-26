d87cbb5961ac76a0298a6f4f1498130cab1c127e
Mark <138724906+mlenda000@users.noreply.github.com>
2023-07-26 09:09:15 -0400

    removed extra code

--- message.js
+++ message.js
@@ -9,7 +9,7 @@ class Message {
      this.commands = commands;
    }
 }
-let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-let message = new Message('Test message with two commands', commands);
+// let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+// let message = new Message('Test message with two commands', commands);
 
 module.exports = Message;
--- rover.js
+++ rover.js
@@ -36,10 +36,10 @@ class Rover {
   } 
 }
 
-let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-let message = new Message('Test message with two commands', commands);
-let rover = new Rover(98382);    // Passes 98382 as the rover's position.
-let response = rover.receiveMessage(message);
-console.log(response);
+// let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+// let message = new Message('Test message with two commands', commands);
+// let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+// let response = rover.receiveMessage(message);
+// console.log(response);
 
 module.exports = Rover;
